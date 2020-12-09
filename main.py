109427e8089ad3a4d0efc7697d625c9b067d2abb
kharundaian <harun.dayan@mail.com>
2020-12-09 22:16:32 +0600

    prepared images and setup the backgruond

---
+++ main.py
@@ -0,0 +1,37 @@
+import pygame
+
+pygame.init()
+width, height = 800, 500
+win = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Hangman Game !")
+
+images = []
+for i in range(7):
+    image = pygame.image.load("hangman" + str(i) + ".png")
+    images.append(image)
+
+hangman_status = 0
+
+white = (255,255,255)
+
+FPS = 60
+clock = pygame.time.Clock()
+run = True
+
+while run:
+    clock.tick(FPS)
+
+    win.fill(white)
+    win.blit(images[hangman_status], (150,100))
+    pygame.display.update()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            print(pos)
+
+pygame.quit()
+
+
